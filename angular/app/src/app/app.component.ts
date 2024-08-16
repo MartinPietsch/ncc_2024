2d094b3867109a830c2b40ef7c86d448341e5ca9
Martin Pietsch <martin.pietsch.dev@gmail.com>
2024-08-16 20:45:01 +0930

    Angular: Supabase connected and working

--- angular/app/src/app/app.component.ts
+++ angular/app/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SupabaseService } from './supabase.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  todos: any[] = [];
+
+  constructor(private supabaseService: SupabaseService) {}
+
+  async ngOnInit() {
+    await this.loadTodos();
+  }
+
+  async loadTodos() {
+    const { data, error } = await this.supabaseService.getRuns();
+    if (error) {
+      console.error('Error fetching todos:', error);
+    } else {
+      this.todos = data;
+    }
+  }
 }
---
+++ angular/app/src/app/supabase.service.ts
@@ -0,0 +1,21 @@
+
+import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SupabaseService {
+  private supabase: SupabaseClient;
+  constructor() {
+    this.supabase = createClient(
+      environment.supabaseUrl,
+      environment.supabaseKey
+    );
+  }
+
+  getRuns() {
+    return this.supabase.from('vw_runs').select('*');
+  }
+}
