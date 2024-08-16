717fc20b7eff9718052ef5bd8ba081350ce7398a
Martin Pietsch <martin.pietsch.dev@gmail.com>
2024-08-16 21:31:17 +0930

    Fix: Runs component - app.module.ts entry missing

--- angular/app/src/app/app.module.ts
+++ angular/app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SupabaseService } from './supabase.service';
+import { RunsComponent } from './components/runs/runs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RunsComponent
   ],
   imports: [
     BrowserModule,
